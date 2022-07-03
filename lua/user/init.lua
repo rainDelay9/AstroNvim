e499b1703e5c951170451e0365fe7f521c5485ab
orlmatan <orlmatan@amazon.com>
2022-07-03 21:58:44 +0300

    onedark colorscheme + lualine

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -59,7 +59,7 @@ local config = {
 
   -- Set colorscheme
   -- colorscheme = "default_theme"
-  colorscheme = "gruvbox",
+  colorscheme = "onedark",
 
   -- set vim options here (vim.<first_key>.<second_key> =  value)
   options = {
@@ -88,7 +88,7 @@ local config = {
     plugins = { -- enable or disable extra plugin highlighting
       aerial = true,
       beacon = false,
-      bufferline = true,
+      bufferline =  true,
       dashboard = true,
       highlighturl = true,
       hop = false,
@@ -148,9 +148,6 @@ local config = {
         'folke/trouble.nvim'
       },
       {
-        'itchyny/lightline.vim'
-      },
-      {
         'simrat39/rust-tools.nvim',
         config = function()
           require('rust-tools').setup(rust_tools_opts)
@@ -195,6 +192,55 @@ local config = {
       },
       {
         "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, cmd = 'MarkdownPreview',
+      },
+      {
+        'navarasu/onedark.nvim',
+        config = function()
+          require('onedark').setup  {
+            -- Main options --
+            style = 'warmer', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+            transparent = false,  -- Show/hide background
+            term_colors = true, -- Change terminal color as per the selected theme style
+            ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+            cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+            -- toggle theme style ---
+            toggle_style_key = '<leader>ts', -- Default keybinding to toggle
+            toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+            -- Change code style ---
+            -- Options are italic, bold, underline, none
+            -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+            code_style = {
+              comments = 'italic',
+              keywords = 'none',
+              functions = 'none',
+              strings = 'none',
+              variables = 'none'
+            },
+
+            -- Custom Highlights --
+            colors = {}, -- Override default colors
+            highlights = {}, -- Override highlight groups
+
+            -- Plugins Config --
+            diagnostics = {
+              darker = true, -- darker colors for diagnostic
+              undercurl = true,   -- use undercurl instead of underline for diagnostics
+              background = true,    -- use background color for virtual text
+            },
+          }
+        end,
+      },
+      {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', 'navarasu/onedark.nvim', opt = true },
+        config = function()
+          require('lualine').setup {
+            options = {
+              theme = 'onedark'
+            }
+          }
+        end,
       }
     },
     -- All other entries override the setup() call for default plugins
